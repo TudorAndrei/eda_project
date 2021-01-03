b662b068dfbd0fc7410dd8b3275630f6292befbf
TudorAndrei <tudorandrei.dumitrascu@gmail.com>
2021-01-03 12:10:06 +0200

    New processed data

--- GTB/xgb.py
+++ GTB/xgb.py
@@ -1,20 +1,22 @@
-from sklearn.preprocessing import (
-    MinMaxScaler,
-    Normalizer,
-    StandardScaler,
-    OneHotEncoder,
-)
-from sklearn.pipeline import Pipeline
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import GradientBoostingClassifier
-from sklearn.base import BaseEstimator, TransformerMixin
-from sklearn.metrics import accuracy_score
-from sklearn.metrics import f1_score
-from sklearn.metrics import precision_score
-from sklearn.metrics import recall_score
-from sklearn.compose import ColumnTransformer
+
+import pandas as pd
 
 
-file_path = r""
+file_path = r"../dataset/processed.csv"
 raw_data = pd.read_csv(file_path)
 
+
+X = raw_data.drop(["goal"], axis=1)
+y = raw_data["goal"]
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X,
+    y,
+)
+
+print(X.shape)
+print(len(y))
+
+model = GradientBoostingClassifier()
